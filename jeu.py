c59d5be1860292293dd98f64f048f294995eadf4
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-01-31 11:24:55 +0100

    Ajout de jeu.py pour gérer le corps du jeu

---
+++ jeu.py
@@ -0,0 +1,7 @@
+# Script contenant le corps du jeu
+import pygame # Importation du module pygame pour gérer le jeu
+
+class Jeu:
+    "Corps du jeu"
+    def __init__(self):
+        
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # Script principal du jeu
-import pygame
-pygame.init()
+import pygame # Importation de pygame
+pygame.init() # Initialisation de pygame
